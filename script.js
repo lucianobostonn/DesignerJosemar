89d714ffd649d84ace3bd118908ba9e54dee8997
Luciano Boston <196549370+lucianobostonn@users.noreply.github.com>
2025-08-03 18:35:56 +0100

    ButtonRedirection

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function enviarWhatsApp(){
 
-    document.getElementById("ContatoWhatsAppICon").addEventListener("click", function () {
+    document.getElementById("TextoWhatsAppP").addEventListener("click", function () {
         const url = `https://wa.me/244924945316?text=Gostaria+de+solicitar+um+servi%C3%A7o,+est%C3%A3o+dispon%C3%ADveis+?+%F0%9F%9F%A2`;
         window.open(url, "_blank");
       })
