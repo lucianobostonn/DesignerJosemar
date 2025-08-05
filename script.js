7f31ebc2408078a0fd762540961481699ea9a19d
Luciano Boston <196549370+lucianobostonn@users.noreply.github.com>
2025-08-05 23:18:14 +0100

    New WhatsAppButton

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ function enviarWhatsApp(){
         const url = `https://wa.me/244924945316?text=Gostaria+de+solicitar+um+servi%C3%A7o,+est%C3%A3o+dispon%C3%ADveis+?+%F0%9F%9F%A2`;
         window.open(url, "_blank");
       })
+
+    document.getElementById("homeTextoWhatsAppP").addEventListener("click", function () {
+        const url = `https://wa.me/244924945316?text=Gostaria+de+solicitar+um+servi%C3%A7o,+est%C3%A3o+dispon%C3%ADveis+?+%F0%9F%9F%A2`;
+        window.open(url, "_blank");
+      })
 }
 
 enviarWhatsApp()
